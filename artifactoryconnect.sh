11809800d165fbc41a4d9298945ac8dc679a1798
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 20:40:58 +0000

    Updated artifactoryconnect.sh

--- artifactoryconnect.sh
+++ artifactoryconnect.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo This is $1 \
-##
-echo This is $test
+git clone "https://$azure_username:azure_password@guycarp.visualstudio.com/GC%20Integration/_git/GC%20Integration" \
+chmod +x "$System.DefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux" \
+"$System.DefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$Build.BuildNumber" --password "$iicscred.Password" --podHostName "dm-us.informaticacloud.com" --region "us" --username "$iicscred.Username" --zipFilePath "$(System.ArtifactsDirectory)/Package_$Build.BuildNumber.zip" \
+rm -R -f "GC%20Integration"
