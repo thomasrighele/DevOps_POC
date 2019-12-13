d46eb3b010b9bded3cc60af466e05358dfdf309e
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 21:15:44 +0000

    Updated test.sh

--- test.sh
+++ test.sh
@@ -4,3 +4,5 @@
 git clone "https://$azure_username:$azure_password@guycarp.visualstudio.com/GC%20Integration/_git/GC%20Integration"
 
 chmod +x "$SystemDefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux"
+
+dir $SystemDefaultWorkingDirectory
