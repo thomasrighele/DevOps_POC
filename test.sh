47bdf68e4e7cbbc04b62a7bf0f6e2d75d19dcb30
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 21:14:00 +0000

    Added test.sh to /

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#My first script
+
+git clone "https://$azure_username:$azure_password@guycarp.visualstudio.com/GC%20Integration/_git/GC%20Integration"
+
+chmod +x "$SystemDefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux"
