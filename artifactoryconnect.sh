51283892319b4687488d7255fc1150dcf773707f
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 20:57:54 +0000

    Updated artifactoryconnect.sh

--- artifactoryconnect.sh
+++ artifactoryconnect.sh
@@ -1,2 +1,1 @@
-git clone "https://$azure_username:$azure_password@guycarp.visualstudio.com/GC%20Integration/_git/GC%20Integration"
-dir $SystemDefaultWorkingDirectory/GC%20Integration
+dir $SystemDefaultWorkingDirectory
