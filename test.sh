3942c59de72d4901c009357577a4628a30065704
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 21:19:07 +0000

    Updated test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #My first script
 
+curl -k -u "$artifactory_username:$artifactory_password" --remote-name "https://artifactory.nasa.azu.mrshmc.com/artifactory/gciics/DevOps_POC/Package_$BuildBuildNumber.zip" -o "$SystemArtifactsDirectory"
+
 git clone "https://$azure_username:$azure_password@guycarp.visualstudio.com/GC%20Integration/_git/GC%20Integration"
 
 chmod +x "$SystemDefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux"
