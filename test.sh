263131cce63acdd586634d3900f1d8dd5109b91d
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 21:22:18 +0000

    Updated test.sh

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ git clone "https://$azure_username:$azure_password@guycarp.visualstudio.com/GC%2
 
 chmod +x "$SystemDefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux"
 
-"$SystemDefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$BuildBuildNumber" --password "$iicscredPassword" --podHostName "dmr-us.informaticacloud.com" --region "us" --username "$iicscredUsername" --zipFilePath "$SystemArtifactsDirectory/Package_$BuildBuildNumber.zip"\
+"$SystemDefaultWorkingDirectory/GC%20Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$BuildBuildNumber" --password "$iicscredPassword" --podHostName "dmr-us.informaticacloud.com" --region "us" --username "$iicscredUsername" --zipFilePath "$SystemArtifactsDirectory/Package_$BuildBuildNumber.zip"
 
 rm -R -f "GC%20Integration"
