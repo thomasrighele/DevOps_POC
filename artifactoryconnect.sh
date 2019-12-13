bd548c6e199d6fb90609e9886e5c65473a25859b
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-13 17:51:11 +0000

    Updated artifactoryconnect.sh

--- artifactoryconnect.sh
+++ artifactoryconnect.sh
@@ -4,6 +4,6 @@ git clone "https://$5:$6@guycarp.visualstudio.com/GC%20Integration/_git/GC%20Int
 
 chmod +x "$7/GC%20Integration/IICS Asset Management Utility/iics_linux"
 
-"$(System.DefaultWorkingDirectory)/GC%20Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$8" --password "$9" --podHostName "dm-us.informaticacloud.com" --region "us" --username "$10" --zipFilePath "$(System.ArtifactsDirectory)/Package_$3.zip"
+"$7/GC%20Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$8" --password "$9" --podHostName "dm-us.informaticacloud.com" --region "us" --username "$10" --zipFilePath "$4/Package_$3.zip"
 
 rm -R -f "GC%20Integration"
