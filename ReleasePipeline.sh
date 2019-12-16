925814cfa035e4ca0b7b14bf32d43c9e33e1fbef
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-16 21:27:29 +0000

    Updated ReleasePipeline.sh

--- ReleasePipeline.sh
+++ ReleasePipeline.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # Azure Release Pipeline Script
 
-#Setting Variables
-env="${ReleaseEnvironmentName// Promotion/}"
-if [ "$env" == PRE ]
- then podHostName="dmr-us.informaticacloud.com"
- else podHostName="dm-us.informaticacloud.com"
-fi
-
-
 # Download Package from Artifactory that is associated with the Build into the Working Directory
 curl -k -u "$artifactory_username:$artifactory_password" --remote-name "https://artifactory.nasa.azu.mrshmc.com/artifactory/gciics/DevOps_POC/Package_$BuildBuildNumber.zip" -o "$SystemArtifactsDirectory"
 
 # Use the IICS Asset Management Utility to import the Artifactory package into IICS
-"$SystemDefaultWorkingDirectory/_GC Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$BuildBuildNumber" --password "$iicscredPassword" --podHostName "$podHostName" --region "us" --username "$iicscredUsername" --zipFilePath "$SystemArtifactsDirectory/Package_$BuildBuildNumber.zip"
+"$SystemDefaultWorkingDirectory/_GC Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$BuildBuildNumber" --password "$iicscredPassword" --podHostName "$iicspodHostName" --region "us" --username "$iicscredUsername" --zipFilePath "$SystemArtifactsDirectory/Package_$BuildBuildNumber.zip"
