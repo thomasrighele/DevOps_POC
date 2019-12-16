78d67ed6e24a1adfc0fdb57fe000bd04a351a5a1
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-16 16:41:24 +0000

    Updated ReleasePipeline.sh

--- ReleasePipeline.sh
+++ ReleasePipeline.sh
@@ -2,7 +2,7 @@
 # Azure Release Pipeline Script
 
 #Setting Variables
-env="${SystemStageName// Promotion/}"
+env="${SystemStageDisplayName// Promotion/}"
 echo $env
 
 # Download Package from Artifactory that is associated with the Build into the Working Directory
