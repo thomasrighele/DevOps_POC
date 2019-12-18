f55bd28793834320524fd1b9bfa48e42702b7474
Thomas Righele <Thomas.Righele@guycarp.com>
2019-12-18 14:43:15 +0000

    Updated RelasePipeline.sh

--- RelasePipeline.sh
+++ RelasePipeline.sh
@@ -2,7 +2,7 @@
 # Azure Release Pipeline Script
 
 # Download Package from Artifactory that is associated with the Build into the Working Directory
-curl -k -u "$artifactory_username:$artifactory_password" --remote-name "https://artifactory.nasa.azu.mrshmc.com/artifactory/gciics/DevOps_POC/Package_$BuildBuildNumber.zip" -o "$SystemArtifactsDirectory"
+curl -k -u "$artifactory_username:$artifactory_password" --remote-name "https://artifactory.nasa.azu.mrshmc.com/artifactory/gciics/$BuildRepositoryName/$BuildRepositoryName_$BuildBuildNumber.zip" -o "$SystemArtifactsDirectory"
 
 # Use the IICS Asset Management Utility to import the Artifactory package into IICS
 "$SystemDefaultWorkingDirectory/_GC Integration/IICS Asset Management Utility/iics_linux" import --name "AzureDevops_Release-$BuildBuildNumber" --password "$iicscredPassword" --podHostName "$iicspodHostName" --region "us" --username "$iicscredUsername" --zipFilePath "$SystemArtifactsDirectory/Package_$BuildBuildNumber.zip"
