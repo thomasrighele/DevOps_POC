cb0739e58bfe341ce090aef745cb05cda3e224e8
Thomas Righele <Thomas.Righele@guycarp.com>
2020-02-12 18:12:56 +0000

    Updated update_Objects.py

--- update_Objects.py
+++ update_Objects.py
@@ -86,7 +86,7 @@ for configuration in attribupdatefile_parsed.iter('configuration'):
 
 
                 ## Update the value within the attributes tag
-                for Entry in Item.findall('types1:Entry'):
+                for Entry in Item.find('Entry', ns):
                     ## Iterate on attributes in attribute file
                     for attributes in connection.iter('attributes'):
                         varname = attributes.find('varname').text
